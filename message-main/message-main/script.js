835033380ba2e56e9837d4b84d6ca101b424f46f
zalmaessam <61655834+zalmaessam@users.noreply.github.com>
2024-02-09 05:22:27 +0200

    Add files via upload

---
+++ message-main/message-main/script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "YIPPIEEEEE!! See you on the 14th :3";
+  gif.src =
+    "https://s3.getstickerpack.com/storage/uploads/sticker-pack/milk-n-mocha-1/sticker_8.gif?3b0113a9484ddf2a389e6bdf5c69c2fa";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
